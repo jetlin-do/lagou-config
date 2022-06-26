64eae6f1c827a5b684cc554c358a5d1801dee9db
jetlin-do <34881399+jetlin-do@users.noreply.github.com>
2022-06-26 08:22:05 +0800

    Update ProductService.java

--- lagou-parent/lagou-service-product/src/main/java/com/lagou/product/service/ProductService.java
+++ lagou-parent/lagou-service-product/src/main/java/com/lagou/product/service/ProductService.java
@@ -9,4 +9,11 @@ public interface ProductService {
      */
     Products queryById(Integer id);
 
+    public void test11();
+    public void test21();
+    public void test31();
+    public void test41();
+    public void test51();
+    public void test61();
+    
 }
